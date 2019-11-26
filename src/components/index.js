f7fa494cf0f23291e98b473dd90e83588896ca4d
lifoogo <aleksandr111111111111111111111@gmail.com>
2019-11-26 23:01:39 +0600

    new header

---
+++ src/components/index.js
@@ -0,0 +1,38 @@
+import React,{Component} from 'react';
+import {Menu,Icon} from 'antd';
+import './header.css';
+
+
+class Header extends Component {
+	constructor(){
+		super();
+	}
+
+	render(){
+		return(
+			
+			<Menu mode='horizontal'>
+				<Menu.Item key="1">
+				<Icon type="aliwangwang" />
+					home
+				</Menu.Item>
+
+				<Menu.Item key="2">
+                <Icon type="qq" />
+					blog
+				</Menu.Item>	
+
+				<Menu.Item key="3">
+                <Icon type="yuque" />
+					Gallery
+				</Menu.Item>
+
+				<Menu.Item key="4">
+                <Icon type="crown" />
+					help
+				</Menu.Item>		
+				</Menu>	
+		)
+	}
+}
+export default Header
